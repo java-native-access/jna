a234334f697d7342c07de9ca8c967350dffbd827
Timothy Wall <twalljava@java.net>
2007-10-27 22:24:04 +0000

    Fix javadoc error
    git-svn-id: https://svn.java.net/svn/jna~svn/trunk@343 2f8a963e-d2e4-e7d0-97bf-ccb7fcea9d80

--- jnalib/src/com/sun/jna/examples/WindowUtils.java
+++ jnalib/src/com/sun/jna/examples/WindowUtils.java
@@ -103,7 +103,7 @@ import com.sun.jna.ptr.PointerByReference;
  * before the corresponding native Drawable may be obtained; in later
  * Java versions, the window need only be displayable.
  * <p>
- * NOTE: If you use {@link #setWindowMask(Shape)} and override {@link
+ * NOTE: If you use {@link #setWindowMask(Window,Shape)} and override {@link
  * Window#paint(Graphics)} on OS X, you'll need to explicitly set the clip
  * mask on the <code>Graphics</code> object with the window mask; only the
  * content pane of the window and below have the window mask automatically
