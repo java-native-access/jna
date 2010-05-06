630974152247f100ece4d44f10c3721bb4599fbf
Anthony Green <green@gmachine.(none)>
2010-05-05 20:14:56 -0400

    Micharl Kohler's spelling fixes

--- .pc/spelling/src/dlmalloc.c
+++ .pc/spelling/src/dlmalloc.c
@@ -4251,7 +4251,7 @@ void* dlmalloc(size_t bytes) {
 
 void dlfree(void* mem) {
   /*
-     Consolidate freed chunks with preceding or succeeding bordering
+     Consolidate freed chunks with preceeding or succeeding bordering
      free chunks, if they exist, and then place in a bin.  Intermixed
      with special cases for top, dv, mmapped chunks, and usage errors.
   */
