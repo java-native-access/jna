10ea848900bc3018ac213cef52b44cacbe5cbebc
Anthony Green <green@moxielogic.com>
2010-08-05 08:24:27 -0400

    don't copy win64 struct args

--- .pc/win64-struct-args/src/x86/ffi.c
+++ .pc/win64-struct-args/src/x86/ffi.c
@@ -291,8 +291,27 @@ void ffi_call(ffi_cif *cif, void (*fn)(void), void *rvalue, void **avalue)
     {
 #ifdef X86_WIN64
     case FFI_WIN64:
-      ffi_call_win64(ffi_prep_args, &ecif, cif->bytes,
-                     cif->flags, ecif.rvalue, fn);
+      {
+        /* Make copies of all struct arguments
+           NOTE: not sure if responsibility should be here or in caller */
+        unsigned int i;
+        for (i=0; i < cif->nargs;i++) {
+          size_t size = cif->arg_types[i]->size;
+          if ((cif->arg_types[i]->type == FFI_TYPE_STRUCT
+               && (size != 1 && size != 2 && size != 4 && size != 8))
+#if FFI_TYPE_LONGDOUBLE != FFI_TYPE_DOUBLE
+              || cif->arg_types[i]->type == FFI_TYPE_LONGDOUBLE
+#endif
+              )
+            {
+              void *local = alloca(size);
+              memcpy(local, avalue[i], size);
+              avalue[i] = local;
+            }
+        }
+        ffi_call_win64(ffi_prep_args, &ecif, cif->bytes,
+                       cif->flags, ecif.rvalue, fn);
+      }
       break;
 #elif defined(X86_WIN32)
     case FFI_SYSV:
