b3de77c961ce1b99e54cb0dcfa49cb389d9228ba
Timothy Wall <twalljava@java.net>
2011-10-06 09:16:32 -0400

    bump native version

--- src/com/sun/jna/Native.java
+++ src/com/sun/jna/Native.java
@@ -83,7 +83,7 @@ import com.sun.jna.Structure.FFIType;
 public final class Native {
 
     private static final String VERSION = "3.3.0";
-    private static final String VERSION_NATIVE = "3.3.0";
+    private static final String VERSION_NATIVE = "3.4.0";
 
     // Used by tests, do not remove
     private static String nativeLibraryPath = null;
