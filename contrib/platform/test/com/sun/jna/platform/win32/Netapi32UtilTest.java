42c9dea2c230fa7028247718e27892480d62f9c1
Daniel Doubrovkine <dblock@dblock.org>
2012-05-15 07:44:36 -0400

    Updated tests: skip when not joined to a DC and check returned SID.

--- contrib/platform/test/com/sun/jna/platform/win32/Netapi32UtilTest.java
+++ contrib/platform/test/com/sun/jna/platform/win32/Netapi32UtilTest.java
@@ -17,6 +17,7 @@ import junit.framework.TestCase;
 import com.sun.jna.platform.win32.Netapi32Util.DomainController;
 import com.sun.jna.platform.win32.Netapi32Util.DomainTrust;
 import com.sun.jna.platform.win32.Netapi32Util.UserInfo;
+import com.sun.jna.platform.win32.Secur32.EXTENDED_NAME_FORMAT;
 
 /**
  * @author dblock[at]dblock[dot]org
@@ -98,16 +99,16 @@ public class Netapi32UtilTest extends TestCase {
 	}
 	
 	public void testGetUserInfo() {
-		// Temporarily commenting out because it fails if run on a network with no domain controller due to default behaviour.
-		// testGetUserInfoWithDomainSpecified() demonstrates what might be a better default behaviour when no domain is specified.
-		//UserInfo userInfo = Netapi32Util.getUserInfo(System.getProperty("user.name"));
-		//assertNotNull(userInfo);
+		if (Netapi32Util.getJoinStatus() != LMJoin.NETSETUP_JOIN_STATUS.NetSetupDomainName)
+			return;
+		assertNotNull(Netapi32Util.getUserInfo(Advapi32Util.getUserName()));
 	}
 
 	public void testGetUserInfoWithDomainSpecified() {
-		UserInfo userInfo = Netapi32Util.getUserInfo(System.getProperty("user.name"),
-		                                             System.getenv("USERDOMAIN"));
+		UserInfo userInfo = Netapi32Util.getUserInfo(Advapi32Util.getUserName(), 
+				System.getenv("USERDOMAIN"));
 		assertNotNull(userInfo);
+		assertTrue(Advapi32.INSTANCE.IsValidSid(userInfo.sid));
 	}
     
 	public void testGetGlobalGroups() {
