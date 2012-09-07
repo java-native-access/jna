804795c5f81c15bb93182a65d3813c4675420339
phailwhale22 <phailwhale22@gmail.com>
2012-09-07 18:26:30 -0400

    fix for issue #116 (continued yet again)
    I'm failing hard... ok to merge? Indentation was fixed and I removed the
    try block.

--- contrib/platform/test/com/sun/jna/platform/win32/Advapi32UtilTest.java
+++ contrib/platform/test/com/sun/jna/platform/win32/Advapi32UtilTest.java
@@ -296,15 +296,8 @@ public class Advapi32UtilTest extends TestCase {
 		Advapi32Util.registrySetStringArray(WinReg.HKEY_CURRENT_USER, "Software\\JNA", "StringArray", stringsWritten);
 		String[] emptyArray = new String[0];
 		Advapi32Util.registrySetStringArray(WinReg.HKEY_CURRENT_USER, "Software\\JNA", "EmptyStringArray", emptyArray);
-                Advapi32Util.registrySetBinaryValue(WinReg.HKEY_CURRENT_USER, "Software\\JNA", "EmptyBinary", new byte[0]);
-                
-                TreeMap<String, Object> values = new TreeMap<String, Object>();
-                try {
-                    values = Advapi32Util.registryGetValues(WinReg.HKEY_CURRENT_USER, "Software\\JNA");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                
+		Advapi32Util.registrySetBinaryValue(WinReg.HKEY_CURRENT_USER, "Software\\JNA", "EmptyBinary", new byte[0]);
+		TreeMap<String, Object> values = Advapi32Util.registryGetValues(WinReg.HKEY_CURRENT_USER, "Software\\JNA");
 		assertEquals(7, values.keySet().size());
 		assertEquals("FourtyTwo" + uu, values.get("42" + uu));
 		assertEquals(42, values.get("FourtyTwo" + uu));
