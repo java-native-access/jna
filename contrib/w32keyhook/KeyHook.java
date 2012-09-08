8003de03eca4f3174f73deea1a97ca64d8957ae4
Daniel Doubrovkine <dblock@dblock.org>
2012-09-08 08:40:58 -0400

    Fix: w32keyhook package to make Eclipse happy.

---
+++ contrib/w32keyhook/KeyHook.java
@@ -0,0 +1,67 @@
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinUser;
+import com.sun.jna.platform.win32.WinDef.HMODULE;
+import com.sun.jna.platform.win32.WinDef.LRESULT;
+import com.sun.jna.platform.win32.WinDef.WPARAM;
+import com.sun.jna.platform.win32.WinUser.HHOOK;
+import com.sun.jna.platform.win32.WinUser.KBDLLHOOKSTRUCT;
+import com.sun.jna.platform.win32.WinUser.LowLevelKeyboardProc;
+import com.sun.jna.platform.win32.WinUser.MSG;
+
+/** Sample implementation of a low-level keyboard hook on W32. */
+public class KeyHook {
+    private static volatile boolean quit;
+    private static HHOOK hhk;
+    private static LowLevelKeyboardProc keyboardHook;
+
+    public static void main(String[] args) {
+        final User32 lib = User32.INSTANCE;
+        HMODULE hMod = Kernel32.INSTANCE.GetModuleHandle(null);
+        keyboardHook = new LowLevelKeyboardProc() {
+            public LRESULT callback(int nCode, WPARAM wParam, KBDLLHOOKSTRUCT info) {
+                if (nCode >= 0) {
+                    switch(wParam.intValue()) {
+                    case WinUser.WM_KEYUP:
+                    case WinUser.WM_KEYDOWN:
+                    case WinUser.WM_SYSKEYUP:
+                    case WinUser.WM_SYSKEYDOWN:
+                        System.err.println("in callback, key=" + info.vkCode);
+                        if (info.vkCode == 81) {
+                            quit = true;
+                        }
+                    }
+                }
+                return lib.CallNextHookEx(hhk, nCode, wParam, info.getPointer());
+            }
+        };
+        hhk = lib.SetWindowsHookEx(WinUser.WH_KEYBOARD_LL, keyboardHook, hMod, 0);
+        System.out.println("Keyboard hook installed, type anywhere, 'q' to quit");
+        new Thread() {
+            public void run() {
+                while (!quit) {
+                    try { Thread.sleep(10); } catch(Exception e) { }
+                }
+                System.err.println("unhook and exit");
+                lib.UnhookWindowsHookEx(hhk);
+                System.exit(0);
+            }
+        }.start();
+
+        // This bit never returns from GetMessage
+        int result;
+        MSG msg = new MSG();
+        while ((result = lib.GetMessage(msg, null, 0, 0)) != 0) {
+            if (result == -1) {
+                System.err.println("error in get message");
+                break;
+            }
+            else {
+                System.err.println("got message");
+                lib.TranslateMessage(msg);
+                lib.DispatchMessage(msg);
+            }
+        }
+        lib.UnhookWindowsHookEx(hhk);
+    }
+}
