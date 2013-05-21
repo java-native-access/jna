d90f6e3346d484e61dd21b363caf876553ef9701
Timothy Wall <twalljava@java.net>
2013-05-21 14:02:48 -0400

    fix typo

--- src/com/sun/jna/Structure.java
+++ src/com/sun/jna/Structure.java
@@ -1303,7 +1303,7 @@ public abstract class Structure {
         else if (actualAlignType == ALIGN_GNUC) {
             // NOTE this is published ABI for 32-bit gcc/linux/x86, osx/x86,
             // and osx/ppc.  osx/ppc special-cases the first element
-            if (!isFirstElement || !(Platform.isMac() && Platfomr.isPPC())) {
+            if (!isFirstElement || !(Platform.isMac() && Platform.isPPC())) {
                 alignment = Math.min(MAX_GNUC_ALIGNMENT, alignment);
             }
             if (!isFirstElement && Platform.isAIX() && (type == double.class || type == Double.class)) {
