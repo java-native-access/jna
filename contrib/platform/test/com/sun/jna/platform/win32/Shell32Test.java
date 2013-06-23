e0d20798028aeac6f5978404443a284a42ae3e40
bsorrentino <bartolomeo.sorrentino@gmail.com>
2013-06-23 14:35:58 -0700

    Add support of function SHAppBarMessage from ShellApi.

--- contrib/platform/test/com/sun/jna/platform/win32/Shell32Test.java
+++ contrib/platform/test/com/sun/jna/platform/win32/Shell32Test.java
@@ -15,13 +15,21 @@ package com.sun.jna.platform.win32;
 import junit.framework.TestCase;
 
 import com.sun.jna.Native;
+import com.sun.jna.platform.win32.ShellAPI.APPBARDATA;
+import com.sun.jna.platform.win32.WinDef.DWORD;
+import com.sun.jna.platform.win32.WinDef.RECT;
+import com.sun.jna.platform.win32.WinDef.UINT_PTR;
 import com.sun.jna.ptr.PointerByReference;
+
 
 /**
  * @author dblock[at]dblock[dot]org
  * @author markus[at]headcrashing[dot]eu
  */
 public class Shell32Test extends TestCase {
+
+    private static final int RESIZE_HEIGHT = 500;
+    private static final int WM_USER = 0x0400;
 
     public static void main(String[] args) {
         junit.textui.TestRunner.run(Shell32Test.class);
@@ -48,4 +56,75 @@ public class Shell32Test extends TestCase {
         assertTrue(Shell32.INSTANCE.SHGetSpecialFolderPath(null, pszPath, ShlObj.CSIDL_APPDATA, false));
         assertFalse(Native.toString(pszPath).isEmpty());
     }
+
+    
+    private void newAppBar() {
+        APPBARDATA data = new APPBARDATA.ByReference();
+        data.cbSize.setValue(data.size());
+        data.uCallbackMessage.setValue(WM_USER + 1);
+
+        UINT_PTR result = Shell32.INSTANCE.SHAppBarMessage(new DWORD(ShellAPI.ABM_NEW), data);
+        assertNotNull(result);
+    }
+
+    private void removeAppBar() {
+        APPBARDATA data = new APPBARDATA.ByReference();
+        data.cbSize.setValue(data.size());
+        UINT_PTR result = Shell32.INSTANCE.SHAppBarMessage(new DWORD(ShellAPI.ABM_REMOVE), data);
+        assertNotNull(result);
+
+    }
+
+    private void queryPos(APPBARDATA data) {
+        UINT_PTR h = Shell32.INSTANCE.SHAppBarMessage(new DWORD(ShellAPI.ABM_QUERYPOS), data);
+
+        assertNotNull(h);
+        assertTrue(h.intValue() > 0);
+
+    }
+
+    public void testResizeDesktopFromBottom() throws InterruptedException {
+
+        newAppBar();
+
+        APPBARDATA data = new APPBARDATA.ByReference();
+
+        data.uEdge.setValue(ShellAPI.ABE_BOTTOM);
+        data.rc.top = User32.INSTANCE.GetSystemMetrics(User32.SM_CYFULLSCREEN) - RESIZE_HEIGHT;
+        data.rc.left = 0;
+        data.rc.bottom = User32.INSTANCE.GetSystemMetrics(User32.SM_CYFULLSCREEN);
+        data.rc.right = User32.INSTANCE.GetSystemMetrics(User32.SM_CXFULLSCREEN);
+
+        queryPos(data);
+
+        UINT_PTR h = Shell32.INSTANCE.SHAppBarMessage(new DWORD(ShellAPI.ABM_SETPOS), data);
+
+        assertNotNull(h);
+        assertTrue(h.intValue() >= 0);
+
+        removeAppBar();
+    }
+
+    public void testResizeDesktopFromTop() throws InterruptedException {
+        
+        newAppBar();
+
+        APPBARDATA data = new APPBARDATA.ByReference();
+        data.uEdge.setValue(ShellAPI.ABE_TOP);
+        data.rc.top = 0;
+        data.rc.left = 0;
+        data.rc.bottom = RESIZE_HEIGHT;
+        data.rc.right = User32.INSTANCE.GetSystemMetrics(User32.SM_CXFULLSCREEN);
+
+        queryPos(data);
+
+        UINT_PTR h = Shell32.INSTANCE.SHAppBarMessage(new DWORD(ShellAPI.ABM_SETPOS), data);
+
+        assertNotNull(h);
+        assertTrue(h.intValue() >= 0);
+
+        removeAppBar();
+
+    }
+
 }
