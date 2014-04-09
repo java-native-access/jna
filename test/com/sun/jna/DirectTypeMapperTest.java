deff187509a5b9736b69a29084136229c88677e4
Taco <marco@mtsystems.ch>
2014-04-09 18:49:14 +0200

    removed unused imports

--- test/com/sun/jna/DirectTypeMapperTest.java
+++ test/com/sun/jna/DirectTypeMapperTest.java
@@ -13,11 +13,6 @@
 
 package com.sun.jna;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 import junit.framework.TestCase;
