5bf7c34005cbf67bd4a658cd0be9e5fafdc39aa4
Michal Junak <junak-michal@countermail.com>
2014-11-17 13:57:21 +0100

    Yet another fix to license header of WTypesTest.java file.

--- contrib/platform/test/com/sun/jna/platform/win32/WTypesTest.java
+++ contrib/platform/test/com/sun/jna/platform/win32/WTypesTest.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011 Timothy Wall, All Rights Reserved
+/* Copyright (c) 2007-2014 Timothy Wall, All Rights Reserved
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
