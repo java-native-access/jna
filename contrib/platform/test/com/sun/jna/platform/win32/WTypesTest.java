a34e28c3b432825062aad399e453b1037a13a200
Michal Junak <junak-michal@countermail.com>
2014-11-17 13:11:39 +0100

    Mentioned LPWSTR and LPSTR memory allocation fix in CHANGES.md. Fixed license header in WTypesTest.java.

--- contrib/platform/test/com/sun/jna/platform/win32/WTypesTest.java
+++ contrib/platform/test/com/sun/jna/platform/win32/WTypesTest.java
@@ -1,10 +1,10 @@
-/* Copyright (c) 2007-2013 Timothy Wall, All Rights Reserved
- *
+/* Copyright (c) 2011 Timothy Wall, All Rights Reserved
+ * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * <p/>
+ * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -18,15 +18,15 @@ import com.sun.jna.Pointer;
 import junit.framework.TestCase;
 
 public class WTypesTest extends TestCase {
-    
+
     private static final String TEST_STRING = "input";
-    
+
     private static final Pointer TEST_POINTER = new Memory((TEST_STRING.length() + 1L) * Native.WCHAR_SIZE);
-    
+
     static {
         TEST_POINTER.setWideString(0, TEST_STRING);
     }
-    
+
     public void testLPOLESTRConstruction() {
         WTypes.LPOLESTR fromString = new WTypes.LPOLESTR(TEST_STRING);
         assertEquals(fromString.getValue(), TEST_STRING);
@@ -35,7 +35,7 @@ public class WTypesTest extends TestCase {
         WTypes.LPOLESTR fromPointer = new WTypes.LPOLESTR(TEST_POINTER);
         assertEquals(fromPointer.getValue(), TEST_STRING);
     }
-    
+
     public void testLPSTRConstruction() {
         WTypes.LPSTR instance = new WTypes.LPSTR(TEST_STRING);
         assertEquals(instance.getValue(), TEST_STRING);
@@ -44,7 +44,7 @@ public class WTypesTest extends TestCase {
         WTypes.LPSTR fromPointer = new WTypes.LPSTR(TEST_POINTER);
         assertEquals(fromPointer.getValue(), TEST_STRING);
     }
-    
+
     public void testLPWSTRConstruction() {
         WTypes.LPWSTR instance = new WTypes.LPWSTR(TEST_STRING);
         assertEquals(instance.getValue(), TEST_STRING);
@@ -53,7 +53,7 @@ public class WTypesTest extends TestCase {
         WTypes.LPWSTR fromPointer = new WTypes.LPWSTR(TEST_POINTER);
         assertEquals(fromPointer.getValue(), TEST_STRING);
     }
-    
+
     public static void main(String[] args) {
         junit.textui.TestRunner.run(WTypesTest.class);
     }
