951de1aff244eaa5839990dc3430f1b58ecff62e
Timothy Wall <twalljava@java.net>
2015-04-19 07:20:46 -0400

    merge from upstream libffi 3.2.1

--- native/libffi/src/s390/ffitarget.h
+++ native/libffi/src/s390/ffitarget.h
@@ -52,6 +52,8 @@ typedef enum ffi_abi {
 } ffi_abi;
 #endif
 
+#define FFI_TARGET_SPECIFIC_STACK_SPACE_ALLOCATION
+#define FFI_TARGET_HAS_COMPLEX_TYPE
 
 /* ---- Definitions for closures ----------------------------------------- */
 
