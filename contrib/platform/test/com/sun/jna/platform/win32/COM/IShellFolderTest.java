8fe525184034782915759be0b99aebd045766cf5
Lauri W Ahonen <lauri.ahonen@iki.fi>
2015-10-24 13:22:07 -0700

    Added assertions for COM success status

--- contrib/platform/test/com/sun/jna/platform/win32/COM/IShellFolderTest.java
+++ contrib/platform/test/com/sun/jna/platform/win32/COM/IShellFolderTest.java
@@ -1,9 +1,10 @@
 package com.sun.jna.platform.win32.COM;
 
 /*
- * Copyright (c) 2015 L W Ahonen, All Rights Reserved
- *
+ * @author L W Ahonen, lwahonen@iki.fi
  */
+
+import junit.framework.TestCase;
 
 
 import com.sun.jna.Pointer;
@@ -19,21 +20,19 @@ public class IShellFolderTest extends TestCase {
         WinNT.HRESULT hr;
         PointerByReference pidl = new PointerByReference();
         hr = Shell32.INSTANCE.SHGetSpecialFolderLocation(null, csidl, pidl);
-        if (COMUtils.SUCCEEDED(hr)) {
-            PointerByReference psfDesktopPTR = new PointerByReference();
-            hr = Shell32.INSTANCE.SHGetDesktopFolder(psfDesktopPTR);
-            if (COMUtils.SUCCEEDED(hr)) {
-                IShellFolder psfDesktop = IShellFolder.Converter.PointerToIShellFolder(psfDesktopPTR);
-                short cb = pidl.getValue().getShort(0);
-                if (cb != 0) {
-                    hr = psfDesktop.BindToObject(pidl.getValue(), null, riid, ppv);
-                } else {
-                    hr = psfDesktop.QueryInterface(riid, ppv);
-                }
-                psfDesktop.Release();
-            }
-            Ole32.INSTANCE.CoTaskMemFree(pidl.getValue());
+        assertTrue(COMUtils.SUCCEEDED(hr));
+        PointerByReference psfDesktopPTR = new PointerByReference();
+        hr = Shell32.INSTANCE.SHGetDesktopFolder(psfDesktopPTR);
+        assertTrue(COMUtils.SUCCEEDED(hr));
+        IShellFolder psfDesktop = IShellFolder.Converter.PointerToIShellFolder(psfDesktopPTR);
+        short cb = pidl.getValue().getShort(0); // See http://blogs.msdn.com/b/oldnewthing/archive/2011/08/30/10202076.aspx for explanation about this bit
+        if (cb != 0) {
+            hr = psfDesktop.BindToObject(pidl.getValue(), null, riid, ppv);
+        } else {
+            hr = psfDesktop.QueryInterface(riid, ppv);
         }
+        psfDesktop.Release();
+        Ole32.INSTANCE.CoTaskMemFree(pidl.getValue());
         return hr;
     }
 
@@ -41,15 +40,11 @@ public class IShellFolderTest extends TestCase {
         Ole32.INSTANCE.CoInitialize(null);
         int CSIDL_DRIVES = 0x0011;
         WinNT.HRESULT hr = Ole32.INSTANCE.CoInitialize(null);
-        if (COMUtils.SUCCEEDED(hr)) {
-            PointerByReference psfMyComputerPTR = new PointerByReference(Pointer.NULL);
-            hr = BindToCsidl(CSIDL_DRIVES, new Guid.REFIID(IShellFolder.IID_ISHELLFOLDER), psfMyComputerPTR);
-            if (COMUtils.SUCCEEDED(hr)) {
-                psfMyComputer = IShellFolder.Converter.PointerToIShellFolder(psfMyComputerPTR);
-            }
-        }
-        if(psfMyComputer == null)
-            throw new RuntimeException("Unable to create my computer shell object");
+        assertTrue(COMUtils.SUCCEEDED(hr));
+        PointerByReference psfMyComputerPTR = new PointerByReference(Pointer.NULL);
+        hr = BindToCsidl(CSIDL_DRIVES, new Guid.REFIID(IShellFolder.IID_ISHELLFOLDER), psfMyComputerPTR);
+        assertTrue(COMUtils.SUCCEEDED(hr));
+        psfMyComputer = IShellFolder.Converter.PointerToIShellFolder(psfMyComputerPTR);
     }
 
     public void tearDown() throws Exception {
@@ -61,31 +56,27 @@ public class IShellFolderTest extends TestCase {
         PointerByReference peidlPTR = new PointerByReference();
         int SHCONTF_FOLDERS = 0x20;
         int SHCONTF_NONFOLDERS = 0x40;
-        boolean sawNames=false;
+        boolean sawNames = false;
 
         WinNT.HRESULT hr = psfMyComputer.EnumObjects(null,
                 SHCONTF_FOLDERS | SHCONTF_NONFOLDERS, peidlPTR);
-        if (COMUtils.SUCCEEDED(hr)) {
-            IEnumIDList peidl = IEnumIDList.Converter.PointerToIEnumIDList(peidlPTR);
-            PointerByReference pidlItem = new PointerByReference();
-            while (peidl.Next(1, pidlItem, null).intValue() == COMUtils.S_OK) {
-                PointerByReference sr = new PointerByReference();
-                hr = psfMyComputer.GetDisplayNameOf(pidlItem.getValue(), 0, sr);
-                if (COMUtils.SUCCEEDED(hr)) {
-                    PointerByReference pszName = new PointerByReference();
-                    hr = Shlwapi.INSTANCE.StrRetToStr(sr, pidlItem.getValue(), pszName);
-                    if (COMUtils.SUCCEEDED(hr)) {
-                        String wideString = pszName.getValue().getWideString(0);
-                        if (wideString != null && wideString.length() > 0)
-                            sawNames = true;
-                        Ole32.INSTANCE.CoTaskMemFree(pszName.getValue());
-                    }
-                    Ole32.INSTANCE.CoTaskMemFree(sr.getValue());
-                }
-                Ole32.INSTANCE.CoTaskMemFree(pidlItem.getValue());
-            }
-            peidl.Release();
+        assertTrue(COMUtils.SUCCEEDED(hr));
+        IEnumIDList peidl = IEnumIDList.Converter.PointerToIEnumIDList(peidlPTR);
+        PointerByReference pidlItem = new PointerByReference();
+        while (peidl.Next(1, pidlItem, null).intValue() == COMUtils.S_OK) {
+            PointerByReference sr = new PointerByReference();
+            hr = psfMyComputer.GetDisplayNameOf(pidlItem.getValue(), 0, sr);
+            assertTrue(COMUtils.SUCCEEDED(hr));
+            PointerByReference pszName = new PointerByReference();
+            hr = Shlwapi.INSTANCE.StrRetToStr(sr, pidlItem.getValue(), pszName);
+            assertTrue(COMUtils.SUCCEEDED(hr));
+            String wideString = pszName.getValue().getWideString(0);
+            if (wideString != null && wideString.length() > 0)
+                sawNames = true;
+            Ole32.INSTANCE.CoTaskMemFree(pszName.getValue());
+            Ole32.INSTANCE.CoTaskMemFree(pidlItem.getValue());
         }
-        assertTrue(sawNames);
+        peidl.Release();
+        assertTrue(sawNames); // We should see at least one item with a name
     }
 }
