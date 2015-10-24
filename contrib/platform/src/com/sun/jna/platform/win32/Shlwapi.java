e792f2f95a4b43c15db5cd932dd9b76b9adf3832
Lauri W Ahonen <lauri.ahonen@iki.fi>
2015-10-24 13:23:24 -0700

    Added documentation

--- contrib/platform/src/com/sun/jna/platform/win32/Shlwapi.java
+++ contrib/platform/src/com/sun/jna/platform/win32/Shlwapi.java
@@ -1,9 +1,7 @@
 package com.sun.jna.platform.win32;
 
-
 /*
- * Copyright (c) 2015 L W Ahonen, All Rights Reserved
- *
+ * @author L W Ahonen, lwahonen@iki.fi
  */
 
 import com.sun.jna.Native;
@@ -13,8 +11,27 @@ import com.sun.jna.platform.win32.WinNT;
 import com.sun.jna.ptr.PointerByReference;
 import com.sun.jna.win32.W32APIOptions;
 
-public interface Shlwapi   extends WinNT {
+public interface Shlwapi extends WinNT {
     Shlwapi INSTANCE = (Shlwapi) Native.loadLibrary("Shlwapi", Shlwapi.class, W32APIOptions.UNICODE_OPTIONS);
+
+
+    /**
+     * Takes an STRRET structure returned by IShellFolder::GetDisplayNameOf and returns a pointer
+     * to an allocated string containing the display name.
+     *
+     * @param pstr
+     *            A pointer to the STRRET structure. When the function returns,
+     *            this pointer will no longer be valid.
+     * @param pidl
+     *            A pointer to the item's ITEMIDLIST structure. This value can be NULL.
+     *
+     * @param ppszName
+     *            A pointer to an allocated string containing the result. StrRetToStr allocates
+     *            memory for this string with CoTaskMemAlloc. You should free the string
+     *            with CoTaskMemFree when it is no longer needed.
+     *
+     * @return If this function succeeds, it returns S_OK. Otherwise, it returns an HRESULT error code.
+     */
 
     HRESULT StrRetToStr(PointerByReference pstr, Pointer pidl, PointerByReference ppszName);
 }
