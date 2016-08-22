f3001dcf8802260c29fcc5e9ae3dc75eefbe6048
Matthias Bläsing <mblaesing@doppel-helix.eu>
2016-08-22 22:19:41 +0200

    Bind DDEML (Dynamic Data Exchange Management Library)

--- contrib/platform/src/com/sun/jna/platform/win32/WinNT.java
+++ contrib/platform/src/com/sun/jna/platform/win32/WinNT.java
@@ -3061,4 +3061,49 @@ public interface WinNT extends WinError, WinDef, WinBase, BaseTSD {
             return FIELDS;
         }
     }
+    
+    public class SECURITY_QUALITY_OF_SERVICE extends Structure {
+
+        public static final List<String> FIELDS = createFieldsOrder(
+                "Length", "ImpersonationLevel", "ContextTrackingMode", "EffectiveOnly"
+        );
+
+        /** Specifies the size, in bytes, of this structure.
+         */
+        public int Length;
+        /**
+         * Specifies the information given to the server about the client, and
+         * how the server may represent, or impersonate, the client. Security
+         * impersonation levels govern the degree to which a server process can
+         * act on behalf of a client process. This member is a
+         * {@link WinNT.SECURITY_IMPERSONATION_LEVEL} enumeration type value.
+         */
+        public int ImpersonationLevel;
+        /**
+         * Specifies whether the server is to be given a snapshot of the
+         * client's security context (called static tracking), or is to be
+         * continually updated to track changes to the client's security context
+         * (called dynamic tracking). The SECURITY_STATIC_TRACKING value
+         * specifies static tracking, and the SECURITY_DYNAMIC_TRACKING value
+         * specifies dynamic tracking. Not all communications mechanisms support
+         * dynamic tracking; those that do not will default to static tracking.
+         */
+        public short ContextTrackingMode;
+        /**
+         * Specifies whether the server may enable or disable privileges and
+         * groups that the client's security context may include.
+         */
+        public BOOL EffectiveOnly;
+
+        @Override
+        public void write() {
+            this.Length = size();
+            super.write();
+        }
+        
+        @Override
+        protected List<String> getFieldOrder() {
+            return FIELDS;
+        }
+    }
 }
