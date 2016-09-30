fffa0773699058e1e7c373236cd6e18b56260992
Matthias Bläsing <mblaesing@doppel-helix.eu>
2016-09-30 21:25:43 +0200

    Adjust file license headers to reflect project license

--- contrib/ntservice/src/jnacontrib/jna/Advapi32.java
+++ contrib/ntservice/src/jnacontrib/jna/Advapi32.java
@@ -1,10 +1,24 @@
 /*
- * Advapi32.java
- *
- * Created on 6. August 2007, 11:24
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ * The contents of this file is dual-licensed under 2 
+ * alternative Open Source/Free licenses: LGPL 2.1 and 
+ * Apache License 2.0. (starting with JNA version 4.0.0).
+ * 
+ * You can freely decide which license you want to apply to 
+ * the project.
+ * 
+ * You may obtain a copy of the LGPL License at:
+ * 
+ * http://www.gnu.org/licenses/licenses.html
+ * 
+ * A copy is also included in the downloadable source code package
+ * containing JNA, in file "LGPL2.1".
+ * 
+ * You may obtain a copy of the Apache License at:
+ * 
+ * http://www.apache.org/licenses/
+ * 
+ * A copy is also included in the downloadable source code package
+ * containing JNA, in file "AL2.0".
  */
 
 package jnacontrib.jna;
