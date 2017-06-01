928182f36c239024e96530776706d17500c6628d
Dan Rollo <danrollo@gmail.com>
2017-06-01 15:55:43 -0400

    add gc before sleep to see if intermittent failures are decreased.

--- test/com/sun/jna/JNALoadTest.java
+++ test/com/sun/jna/JNALoadTest.java
@@ -143,6 +143,7 @@ public class JNALoadTest extends TestCase implements Paths, GCWaits {
         // Check for temporary file deletion
         File f = new File(path);
         for (int i=0;i < GC_WAITS && (f.exists() || Boolean.getBoolean("jna.loaded"));i++) {
+            System.gc(); // attempt to fix intermittent test failures
             Thread.sleep(4 * GC_WAIT_INTERVAL);  // '4 *' is attempt to fix intermittent test failures
             System.gc();
         }
