b3cc591f0fec92b5d56df8f760b640eb12409c77
Matthias Bläsing <mblaesing@doppel-helix.eu>
2017-12-23 17:16:07 +0100

    Derive android versionCode from major/minor/revision

--- ant-tools-src/com/sun/jna/BuildArmSoftFloatDetector.java
+++ ant-tools-src/com/sun/jna/BuildArmSoftFloatDetector.java
@@ -1,3 +1,26 @@
+/* Copyright (c) 2016 Matthias Bläsing, All Rights Reserved
+ *
+ * The contents of this file is dual-licensed under 2
+ * alternative Open Source/Free licenses: LGPL 2.1 or later and
+ * Apache License 2.0. (starting with JNA version 4.0.0).
+ *
+ * You can freely decide which license you want to apply to
+ * the project.
+ *
+ * You may obtain a copy of the LGPL License at:
+ *
+ * http://www.gnu.org/licenses/licenses.html
+ *
+ * A copy is also included in the downloadable source code package
+ * containing JNA, in file "LGPL2.1".
+ *
+ * You may obtain a copy of the Apache License at:
+ *
+ * http://www.apache.org/licenses/
+ *
+ * A copy is also included in the downloadable source code package
+ * containing JNA, in file "AL2.0".
+ */
 
 package com.sun.jna;
 
@@ -8,13 +31,6 @@ import org.apache.tools.ant.Project;
 /**
  * Ant task to expose the arm soft-/hardfloat detection routines of the JNA core
  * for the build script.
- * 
- * <p>The build script is expected to build a minimal set of classes that are 
- * required to execute this. At the time of writing these are:</p>
- * 
- * <ul>
- * <li>com.sun.jna.ELFAnalyser</li>
- * </ul>
  */
 public class BuildArmSoftFloatDetector {
 
