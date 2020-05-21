d4efea443b44c25fd9ffc2b57d12f691ba94bc8f
Daniel Widdis <widdis@gmail.com>
2020-05-21 16:51:48 -0700

    Add Object Oriented convenience functions to udev classes

--- contrib/platform/test/com/sun/jna/platform/linux/UdevTest.java
+++ contrib/platform/test/com/sun/jna/platform/linux/UdevTest.java
@@ -37,74 +37,78 @@ import junit.framework.TestCase;
  */
 public class UdevTest extends TestCase {
 
-    private static final Udev UDEV = Udev.INSTANCE;
-
     @Test
     public void testEnumerateDevices() {
         // Start with the context object
-        UdevContext udev = UDEV.udev_new();
-        assertNotNull("Failed to create udev context", udev);
-        // Create an enumerator
-        UdevEnumerate enumerate = UDEV.udev_enumerate_new(udev);
-        assertNotNull("Failed to create udev enumerator", enumerate);
-        // It's a good assumption the machine we're on has a disk and partition
-        // Add a filter for block devices and scan them
-        UDEV.udev_enumerate_add_match_subsystem(enumerate, "block");
-        UDEV.udev_enumerate_scan_devices(enumerate);
-        // Enumerator now points to block devices.
-
-        // Iterate the list
-        UdevListEntry entry = UDEV.udev_enumerate_get_list_entry(enumerate);
-        assertNotNull("Failed to get an enumerator entry", entry);
-        String entryName = UDEV.udev_list_entry_get_name(entry);
-        assertEquals("Udev entry name should start with /sys", "/sys", entryName.substring(0, 4));
-        UdevDevice device = UDEV.udev_device_new_from_syspath(udev, entryName);
-        while (device != null) {
-            // devnode is what we use as name, like /dev/sda
-            String devnode = UDEV.udev_device_get_devnode(device);
-            // Ignore loopback and ram disks
-            if (devnode != null && !devnode.startsWith("/dev/loop") && !devnode.startsWith("/dev/ram")) {
-                String devType = UDEV.udev_device_get_devtype(device);
-                // For partition, grab values of parent disk
-                String parentSize = null;
-                String parentMajor = null;
-                if ("partition".equals(devType)) {
-                    UdevDevice parent = UDEV.udev_device_get_parent_with_subsystem_devtype(device, "block", "disk");
-                    String parentName = UDEV.udev_device_get_devnode(parent);
-                    // These should match same parent without restricting to block and disk
-                    UdevDevice parent2 = UDEV.udev_device_get_parent(device);
-                    String parentName2 = UDEV.udev_device_get_devnode(parent2);
-                    assertEquals("Partition parent should match with and without filter", parentName, parentName2);
-                    // Save the size and major
-                    parentSize = UDEV.udev_device_get_sysattr_value(parent, "size");
-                    parentMajor = UDEV.udev_device_get_property_value(parent, "MAJOR");
+        UdevContext udev = Udev.INSTANCE.udev_new();
+        try {
+            assertNotNull("Failed to create udev context", udev);
+            // Create an enumerator
+            UdevEnumerate enumerate = udev.enumerateNew();
+            try {
+                assertNotNull("Failed to create udev enumerator", enumerate);
+                // It's a good assumption the machine we're on has a disk and partition
+                // Add a filter for block devices and scan them
+                enumerate.addMatchSubsystem("block");
+                enumerate.scanDevices();
+                // Enumerator now points to block devices.
+                // Iterate the list
+                for (UdevListEntry entry = enumerate.getListEntry(); entry != null; entry = entry.getNext()) {
+                    String syspath = entry.getName();
+                    assertEquals("Udev entry name should start with /sys", "/sys", syspath.substring(0, 4));
+                    UdevDevice device = udev.deviceNewFromSyspath(syspath);
+                    if (device != null) {
+                        try {
+                            // devnode is what we use as name, like /dev/sda
+                            String devnode = device.getDevnode();
+                            // Ignore loopback and ram disks
+                            if (devnode != null && !devnode.startsWith("/dev/loop")
+                                    && !devnode.startsWith("/dev/ram")) {
+                                String devType = device.getDevtype();
+                                // For partition, grab values of parent disk
+                                String parentSize = null;
+                                String parentMajor = null;
+                                if ("partition".equals(devType)) {
+                                    UdevDevice parent = device.getParentWithSubsystemDevtype("block", "disk");
+                                    UdevDevice parent2 = device.getParent();
+                                    // No additional reference is acquired from getParent
+                                    if (parent != null && parent2 != null) {
+                                        // Devnode should match same parent without restricting to block and disk
+                                        assertEquals("Partition parent should match with and without filter",
+                                                parent.getDevnode(), parent2.getDevnode());
+                                        // Save the size and major
+                                        parentSize = parent.getSysattrValue("size");
+                                        parentMajor = parent.getPropertyValue("MAJOR");
+                                    }
+                                }
+                                String size = device.getSysattrValue("size");
+                                assertTrue("Size must be nonnegative", 0 <= Long.parseLong(size));
+                                if (parentSize != null) {
+                                    assertTrue("Partition can't be bigger than its disk",
+                                            Long.parseLong(size) <= Long.parseLong(parentSize));
+                                }
+                                String major = device.getPropertyValue("MAJOR");
+                                assertTrue("Major value must be nonnegative", 0 <= Long.parseLong(major));
+                                if (parentMajor != null) {
+                                    assertEquals("Partition and its parent disk should have same major number", major,
+                                            parentMajor);
+                                }
+                                assertEquals("DevType mismatch", devType, device.getDevtype());
+                                assertEquals("Subsystem mismatch", "block", device.getSubsystem());
+                                assertEquals("Syspath mismatch", syspath, device.getSyspath());
+                                assertTrue("Syspath should end with name", syspath.endsWith(device.getSysname()));
+                            }
+                        } finally {
+                            // Release the reference and iterate to the next device
+                            device.unref();
+                        }
+                    }
                 }
-                String size = UDEV.udev_device_get_sysattr_value(device, "size");
-                assertTrue("Size must be nonnegative", 0 <= Long.parseLong(size));
-                if (parentSize != null) {
-                    assertTrue("Partition can't be bigger than its disk",
-                            Long.parseLong(size) <= Long.parseLong(parentSize));
-                }
-                String major = UDEV.udev_device_get_property_value(device, "MAJOR");
-                assertTrue("Major value must be nonnegative", 0 <= Long.parseLong(major));
-                if (parentMajor != null) {
-                    assertEquals("Partition and its parent disk should have same major number", major, parentMajor);
-                }
-                assertEquals("DevType mismatch", devType, UDEV.udev_device_get_devtype(device));
-                assertEquals("Subsystem mismatch", "block", UDEV.udev_device_get_subsystem(device));
-                assertEquals("Syspath mismatch", entryName, UDEV.udev_device_get_syspath(device));
-                assertTrue("Syspath should end with name", entryName.endsWith(UDEV.udev_device_get_sysname(device)));
+            } finally {
+                enumerate.unref();
             }
-            UDEV.udev_device_unref(device);
-            // Release the reference and iterate to the next device
-            entry = UDEV.udev_list_entry_get_next(entry);
-            entryName = UDEV.udev_list_entry_get_name(entry);
-            if (entry != null) {
-                assertEquals("Udev entry name should start with /sys", "/sys", entryName.substring(0, 4));
-            }
-            device = UDEV.udev_device_new_from_syspath(udev, entryName);
+        } finally {
+            udev.unref();
         }
-        UDEV.udev_enumerate_unref(enumerate);
-        UDEV.udev_unref(udev);
     }
 }
