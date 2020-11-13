3df07ae0cb11ead4e1c175ad395feee3ea1d57fd
Dmitry Prokhorov <nordia@177013.ru>
2020-11-13 11:38:26 +0300

    Integrate w32keyhook & w32mousehook

--- contrib/w32windowhooks/com/sun/jna/contrib/demo/WindowHooks.java
+++ contrib/w32windowhooks/com/sun/jna/contrib/demo/WindowHooks.java
@@ -26,26 +26,32 @@ package com.sun.jna.contrib.demo;
 import com.sun.jna.Pointer;
 import com.sun.jna.platform.win32.Kernel32;
 import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef;
 import com.sun.jna.platform.win32.WinDef.HMODULE;
 import com.sun.jna.platform.win32.WinDef.LRESULT;
 import com.sun.jna.platform.win32.WinDef.WPARAM;
 import com.sun.jna.platform.win32.WinDef.LPARAM;
 import com.sun.jna.platform.win32.WinUser;
-import com.sun.jna.platform.win32.WinUser.HHOOK;
 import com.sun.jna.platform.win32.WinUser.KBDLLHOOKSTRUCT;
 import com.sun.jna.platform.win32.WinUser.LowLevelKeyboardProc;
 import com.sun.jna.platform.win32.WinUser.MSG;
 
-/** Sample implementation of a low-level keyboard hook on W32. */
-public class KeyHook {
+/** Sample implementation of a low-level window hooks on W32. */
+public class WindowHooks {
     private static volatile boolean quit;
-    private static HHOOK hhk;
-    private static LowLevelKeyboardProc keyboardHook;
+
+    private static WinUser.LowLevelMouseProc mouseProc;
+    private static WinUser.HHOOK mouseHook;
+
+    private static WinUser.LowLevelKeyboardProc keyboardProc;
+    private static WinUser.HHOOK keyboardHook;
 
     public static void main(String[] args) {
         final User32 lib = User32.INSTANCE;
+
         HMODULE hMod = Kernel32.INSTANCE.GetModuleHandle(null);
-        keyboardHook = new LowLevelKeyboardProc() {
+
+        keyboardProc = new LowLevelKeyboardProc() {
             @Override
             public LRESULT callback(int nCode, WPARAM wParam, KBDLLHOOKSTRUCT info) {
                 if (nCode >= 0) {
@@ -63,11 +69,60 @@ public class KeyHook {
 
                 Pointer ptr = info.getPointer();
                 long peer = Pointer.nativeValue(ptr);
-                return lib.CallNextHookEx(hhk, nCode, wParam, new LPARAM(peer));
+                return lib.CallNextHookEx(keyboardHook, nCode, wParam, new LPARAM(peer));
             }
         };
-        hhk = lib.SetWindowsHookEx(WinUser.WH_KEYBOARD_LL, keyboardHook, hMod, 0);
+        keyboardHook = lib.SetWindowsHookEx(WinUser.WH_KEYBOARD_LL, keyboardProc, hMod, 0);
+
         System.out.println("Keyboard hook installed, type anywhere, 'q' to quit");
+
+        mouseProc = new WinUser.LowLevelMouseProc() {
+            @Override
+            public LRESULT callback(int nCode, WPARAM wParam, WinUser.MSLLHOOKSTRUCT lParam) {
+                if (nCode >= 0) {
+                    switch (wParam.intValue()) {
+                        case 512: {
+                            System.out.println("Mouse moved!");
+
+                            break;
+                        }
+                        case 513: {
+                            System.out.println("Left button down!");
+
+                            break;
+                        }
+                        case 514: {
+                            System.out.println("Left button up!");
+
+                            break;
+                        }
+                        case 516: {
+                            System.out.println("Right button down!");
+
+                            break;
+                        }
+                        case 517: {
+                            System.out.println("Right button up!");
+
+                            break;
+                        }
+                        case 519: {
+                            System.out.println("Middle button down!");
+
+                            break;
+                        }
+                        case 520: {
+                            System.out.println("Middle button up!");
+
+                            break;
+                        }
+                    }
+                }
+                return lib.CallNextHookEx(mouseHook, nCode, wParam, new WinDef.LPARAM(Pointer.nativeValue(lParam.getPointer())));
+            }
+        };
+        mouseHook = User32.INSTANCE.SetWindowsHookEx(WinUser.WH_MOUSE_LL, mouseProc, hMod, 0);
+
         new Thread() {
             @Override
             public void run() {
@@ -75,7 +130,8 @@ public class KeyHook {
                     try { Thread.sleep(10); } catch(Exception e) { }
                 }
                 System.err.println("unhook and exit");
-                lib.UnhookWindowsHookEx(hhk);
+                lib.UnhookWindowsHookEx(keyboardHook);
+                lib.UnhookWindowsHookEx(mouseHook);
                 System.exit(0);
             }
         }.start();
@@ -94,6 +150,7 @@ public class KeyHook {
                 lib.DispatchMessage(msg);
             }
         }
-        lib.UnhookWindowsHookEx(hhk);
+        lib.UnhookWindowsHookEx(keyboardHook);
+        lib.UnhookWindowsHookEx(mouseHook);
     }
 }
