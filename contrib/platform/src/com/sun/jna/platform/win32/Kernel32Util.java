263f074e15093ce2d70b14db1760f4bb7fcf1911
dEajL3kA <Cumpoing79@web.de>
2023-08-08 11:13:11 +0200

    Refactored error handling.

--- contrib/platform/src/com/sun/jna/platform/win32/Kernel32Util.java
+++ contrib/platform/src/com/sun/jna/platform/win32/Kernel32Util.java
@@ -392,23 +392,10 @@ public abstract class Kernel32Util implements WinDef {
                 default:
                     return type;
             }
-        } catch(Win32Exception e) {
-            err = e;
-            throw err;  // re-throw so finally block executed
+        } catch(final Win32Exception e) {
+            throw err = e; // re-throw to avoid return value!
         } finally {
-            try {
-                closeHandle(hFile);
-            } catch(Win32Exception e) {
-                if (err == null) {
-                    err = e;
-                } else {
-                    err.addSuppressedReflected(e);
-                }
-            }
-
-            if (err != null) {
-                throw err;
-            }
+            cleanUp(hFile, err);
         }
     }
 
@@ -944,22 +931,10 @@ public abstract class Kernel32Util implements WinDef {
                 throw new Win32Exception(Kernel32.INSTANCE.GetLastError());
             }
             return QueryFullProcessImageName(hProcess, dwFlags);
-        } catch (Win32Exception e) {
-            we = e;
-            throw we; // re-throw to invoke finally block
+        } catch (final Win32Exception e) {
+            throw we = e; // re-throw to avoid return value!
         } finally {
-            try {
-                closeHandle(hProcess);
-            } catch (Win32Exception e) {
-                if (we == null) {
-                    we = e;
-                } else {
-                    we.addSuppressedReflected(e);
-                }
-            }
-            if (we != null) {
-                throw we;
-            }
+            cleanUp(hProcess, we);
         }
     }
 
@@ -1223,23 +1198,10 @@ public abstract class Kernel32Util implements WinDef {
             }
 
             return modules;
-        } catch (Win32Exception e) {
-            we = e;
-            throw we;   // re-throw so finally block is executed
+        } catch (final Win32Exception e) {
+            throw we = e; // re-throw to avoid return value!
         } finally {
-            try {
-                closeHandle(snapshot);
-            } catch(Win32Exception e) {
-                if (we == null) {
-                    we = e;
-                } else {
-                    we.addSuppressedReflected(e);
-                }
-            }
-
-            if (we != null) {
-                throw we;
-            }
+            cleanUp(snapshot, we);
         }
     }
 
@@ -1294,5 +1256,26 @@ public abstract class Kernel32Util implements WinDef {
         } else {
             return resultMemory.getString(0);
         }
+    }
+
+    /**
+     * Close the given handle, then possibly throw the pending Win32Exception
+     *
+     * @param we The pending Win32Exception, can be NULL.
+     * @throws Win32Exception if an error occurs, or there was a pending exception.
+     */
+    private static void cleanUp(final HANDLE h, Win32Exception we) {
+        try {
+            closeHandle(h);
+        } catch (final Win32Exception e) {
+            if (we == null) {
+                we = e;
+            } else {
+                we.addSuppressedReflected(e);
+            }
+        }
+        if (we != null) {
+            throw we;
+        } 
     }
 }
